327a0bc945e4d3df4f7f8c9cfe1cecb8a83c91d1
Andreas Fehlmann <andreas.fehlmann@hotmail.com>
2017-09-10 17:29:47 +0200

    Finished it up, formatted it, added some nice comments

--- andisbot.js
+++ andisbot.js
@@ -15,13 +15,8 @@ console.log = function(d) { //
 };
 
 // var token = 'TOKEN HERE'; 
-var token = 'MzUyMjYyOTUzMTU3OTE4NzIx.DIfB5w.BEXI7N5Dziq--5fL2izRR5uQr48';
+var token = 'MzUyMjYyOTUzMTU3OTE4NzIx.DJG6aw.qPPclEb2E1JeL-A4gaEt0WpOIi4';
 
-
-//var truths = "TRUTHS API";
-//var dares = "DARES API";
-var truths = "http://dobij5.com/truths.php";
-var dares = "http://dobij5.com/dares.php";
 var counter = 0;
 var currentPlayer = 0;
 
@@ -46,9 +41,17 @@ function shuffle(array) {
   return array;
 }
 
-function search(nameKey, myArray){
+function search(myArray, nameKey){
     for (var i=0; i < myArray.length; i++) {
         if (myArray[i] === nameKey) {
+            return myArray[i];
+        }
+    }
+}
+
+function searchIndex(myArray, index){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i] === index) {
             return myArray[i];
         }
     }
@@ -59,96 +62,117 @@ var help = "Truth and Dare is a popular game. How it's played is a player asks a
 var info = "The TruthAndDareBot is made by @UnknownDeveloper that power this bot. The bot is licensed under the MIT License. For More information visit the GitHub page at this page https://github.com/TheRealUnknownDeveloper/TruthAndDareBot-Discord-Edition/"
 
 bot.on("ready", function(msg) {
-  console.log("Woop! Bot logged in under the name of " + bot.user.username + " and the user ID of " + bot.user.id)
-})
-bot.on("message", function(msg) {
-	//message comes in
+	console.log("Woops! Bot logged in under the name of " + bot.user.username + " and the user ID of " + bot.user.id)
+	})
 	
-  var myMsg = msg.content.toLowerCase();
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> truth") == 0) {
-	var newMsg = "You called Truth! Player #" + 
-	(currentPlayer+1) + " wants to get a Question!";
-	currentPlayer++;
-	msg.reply(newMsg);
-  }
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> dare") == 0) {
-	var newMsg = "You called Dare! Player #" + 
-	(currentPlayer+1) + " wants to get a Dare!";
-	currentPlayer++;
-	msg.reply(newMsg);
-  }
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> queue") == 0) {
-	  console.log(myMsg);
-	  userList[counter] = myMsg.split(" ")[2];
-	  
-	  counter++;
-	  currentPlayer++;
-	  msg.reply(userList[counter-1] + " joined the game!");
-  }
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> leave") == 0) {
-	  console.log(myMsg);
-	  counter--;
-	  var resultObject = search(myMsg.split(" ")[2], userList);
-	  
-		var filtered = userList.filter(function(el) {
+	//Msg comes in start
+	bot.on("message", function(msg) {
+
+		var oldOGMsg = msg.content;
+		var myMsg = msg.content.toLowerCase();
+
+		if (myMsg.indexOf("<@" + bot.user.id + "> truth") == 0) {
+			var askerName, playerName, newMsg;
+			
+			if(currentPlayer == 0) askerName = userList[userList.length() -1];
+			else askerName = userList[currentPlayer -1];
+			
+			playerName = searchIndex(userList, currentPlayer);
+			newMsg = "You called Truth! Player " + playerName + 
+			" wants to get a Question! Player " + askerName + " please ask him a Question!";
+			currentPlayer++;
+			msg.reply(newMsg);
+		}
+
+		if (myMsg.indexOf("<@" + bot.user.id + "> dare") == 0) {
+			var askerName, playerName, newMsg;
+			
+			if(currentPlayer == 0) askerName = userList[userList.length() -1];
+			else askerName = userList[currentPlayer -1];
+			
+			playerName = searchIndex(userList, currentPlayer);
+			newMsg = "You called Dare! Player " + playerName + 
+			" wants to get a Dare! Player " + askerName + " please give the other player something to do!";
+			currentPlayer++;
+			msg.reply(newMsg);
+			
+			var newMsg = "You called Dare! Player #" + 
+			(currentPlayer+1) + " wants to get a Dare!";
+			currentPlayer++;
+			msg.reply(newMsg);
+		}
+		
+		if (myMsg.indexOf("<@" + bot.user.id + "> join") == 0) {
+		  console.log(oldOGMsg);
+		  userList[counter] = oldOGMsg.split(" ")[2];
+		  
+		  counter++;
+		  currentPlayer++;
+		  msg.reply(userList[counter-1] + " joined the game!");
+		}
+
+		if (myMsg.indexOf("<@" + bot.user.id + "> leave") == 0) {
+			console.log(myMsg);
+			counter--;
+			var resultObject = search(userList, myMsg.split(" ")[2]);
+
+			var filtered = userList.filter(function(el) {
 			return el !== resultObject;
-		});
-	  
-	  userList = filtered;
-	  //list = list + counter + " - " + myMsg.split(" ")[2] + "\n";
-	  msg.reply(userList[counter] + " joined the game!");
-  }
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> show") == 0) {
-	  var reply = "--- Current userlist ---" + "\n";
-	  
-	  for(var i = 1; i < userList.length; i++){
-		  if((i-1) === currentPlayer){
-			  reply = reply + "--> ";
+			});
+
+			userList = filtered;
+			//list = list + counter + " - " + myMsg.split(" ")[2] + "\n";
+			msg.reply(userList[counter] + " joined the game!");
+		}
+
+		if (myMsg.indexOf("<@" + bot.user.id + "> show") == 0) {
+		  var reply = "--- Current userlist ---" + "\n";
+		  
+		  for(var i = 1; i < userList.length; i++){
+			  if((i-1) === currentPlayer){
+				  reply = reply + "--> ";
+			  }
+			  reply = reply + i + ": " + userList[i-1] + "\n";
 		  }
-		  reply = reply + i + ": " + userList[i-1] + "\n";
-	  }
-	  
-	  reply = reply + "--- To randomize use the command shuffle! The player with --> is the current player ---";
-	  msg.reply(reply);
+		  
+		  reply = reply + "--- To randomize use the command shuffle! The player with --> is the current player ---";
+		  msg.reply(reply);
+
+		}
+
+		if (myMsg.indexOf("<@" + bot.user.id + "> shuffle") == 0) {
+		  userList = shuffle(userList);
+		  currentPlayer = 0;
+		  var reply = "The list was shuffled. Use the Show Command to view the current list!";
+		  msg.reply(reply);
+
+		}
+
+		if (myMsg.indexOf("<@" + bot.user.id + "> info") == 0) {
+		  var infomsg =
+		  "Available Commands:\n" +
+		  "Info and Help: Name says it all\n" + 
+		  "Show: Shows the current list of people playing\n" + 
+		  "Join NAME: the person NAME enters the game!\n" +
+		  "Leave NAME: this person leaves the list\n"+
+		  "Truth: The player chooses Truth\n"+
+		  "Dare: The player chooses Dare\n"+
+		  "Shuffle: Shuffles the list randomly. Use this at the end of the round. or let me do it...\n" +
+		  "Clear: Only use this if you want to end the whole game";
+		  msg.reply(infomsg);
+		}
+
+		if (myMsg.indexOf("<@" + bot.user.id + "> help") == 0) {
+		msg.reply("This bot was created by Ezio and only works locally at the moment. Drop him a DM if you need something <3");
+		}
+
+		if (myMsg.indexOf("<@" + bot.user.id + "> clear") == 0) {
+		  userList = [];
+		  counter = 0;
+		  currentPlayer = 0;
+		msg.reply("List got cleared!");
+		}
+	});
+	//Msg comes in end
 	
-  }
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> shuffle") == 0) {
-	  userList = shuffle(userList);
-	  currentPlayer = 0;
-	  var reply = "The list was shuffled. Use the Show Command to view the current list!";
-	  msg.reply(reply);
-	
-  }
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> info") == 0) {
-	  var infomsg =
-	  "Available Commands:\n" +
-	  "Info and Help: Name says it all\n" + 
-	  "Show: Shows the current list of people playing\n" + 
-	  "Queue NAME: the person NAME enters the game!\n" +
-	  "Leave NAME: this person leaves the list\n"+
-	  "Truth: The player chooses Truth\n"+
-	  "Dare: The player chooses Dare\n"+
-	  "Shuffle: Shuffles the list randomly. Use this at the end of the round. or let me do it...\n" +
-	  "Clear: Only use this if you want to end the whole game";
-      msg.reply(infomsg);
-  }
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> help") == 0) {
-    msg.reply("This bot was created by Ezio and only works locally at the moment. Drop him a DM if you need something <3");
-  }
-  
-  if (myMsg.indexOf("<@" + bot.user.id + "> clear") == 0) {
-	  userList = [];
-	  counter = 0;
-	  currentPlayer = 0;
-    msg.reply("List got cleared!");
-  }
-});
 bot.login(token);
